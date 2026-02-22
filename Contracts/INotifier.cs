5106fbac078ee184d87ed950dea3e6b6cd50f78d
athapaliya21 <alexthapaliyaa@gmail.com>
2026-02-21 21:20:34 -0500

    Add notification methods to INotifier interface

--- Contracts/INotifier.cs
+++ Contracts/INotifier.cs
@@ -2,5 +2,6 @@ namespace ITCS_3112_Lab_1_Checkout.Contracts;
 
 public interface INotifier
 {
-    
+    void NotifyDueSoon(CheckoutRecord record);
+    void NotifyOverdue(CheckoutRecord record);
 }
