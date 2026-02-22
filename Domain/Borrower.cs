0c3eda8ee10eb644b7da39a732aa55233c037015
athapaliya21 <alexthapaliyaa@gmail.com>
2026-02-21 21:23:30 -0500

    Add properties and constructor to Borrower class

--- Domain/Borrower.cs
+++ Domain/Borrower.cs
@@ -2,5 +2,16 @@ namespace ITCS_3112_Lab_1_Checkout.Domain;
 
 public class Borrower
 {
-    
+    public string Id { get; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public Borrower(string id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+
+    public override string ToString() => $"{Name} ({Email})";
 }
