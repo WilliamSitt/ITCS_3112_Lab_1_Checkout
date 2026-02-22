a204a190d374cedaea0f5785bcfc34833f093b84
athapaliya21 <alexthapaliyaa@gmail.com>
2026-02-21 21:22:09 -0500

    Add methods to IPolicy interface for checkout logic

--- Contracts/IPolicy.cs
+++ Contracts/IPolicy.cs
@@ -2,5 +2,7 @@ namespace ITCS_3112_Lab_1_Checkout.Contracts;
 
 public interface IPolicy
 {
-    
+    bool CanCheckOut(Item item);
+    int GetMaxLoanDays(Item item);
+    bool IsDueDateValid(Item item, DateTime requestedDueDate);
 }
