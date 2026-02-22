00e8a13ff22b6fd470f225d16764e2d86b8367c3
athapaliya21 <alexthapaliyaa@gmail.com>
2026-02-21 21:19:16 -0500

    Add methods to ICatalog interface for item management

--- Contracts/ICatalog.cs
+++ Contracts/ICatalog.cs
@@ -2,5 +2,10 @@ namespace ITCS_3112_Lab_1_Checkout.Contracts;
 
 public interface ICatalog
 {
-    
+    List<Item> ListAvailable();
+    List<Item> ListUnavailable();
+    List<Item> ListAll();
+    List<Item> SearchById(string itemId);
+    List<Item> SearchByName(string keyword);
+    List<Item> SearchByCategory(string category);
 }
