bae8b1316aa38e7ad04bc757852312e3da875941
athapaliya21 <alexthapaliyaa@gmail.com>
2026-02-21 21:22:29 -0500

    Add methods to IRepository interface

--- Contracts/IRepository.cs
+++ Contracts/IRepository.cs
@@ -2,5 +2,11 @@ namespace ITCS_3112_Lab_1_Checkout.Contracts;
 
 public interface IRepository
 {
-    
+    void AddItem(Item item);
+    Item? GetItem(string itemId);
+    List<Item> GetAllItems();
+    void SaveRecord(CheckoutRecord record);
+    List<CheckoutRecord> GetRecordsByBorrower(string borrowerId);
+    List<CheckoutRecord> GetRecordsByItem(string itemId);
+    List<CheckoutRecord> GetAllRecords();
 }
