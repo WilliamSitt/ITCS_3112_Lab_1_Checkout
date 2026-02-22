7e4cb14086e4ca13eec8f055f47ca58c33495470
athapaliya21 <alexthapaliyaa@gmail.com>
2026-02-21 21:19:43 -0500

    Add methods to ICheckoutService interface

--- Contracts/ICheckoutService.cs
+++ Contracts/ICheckoutService.cs
@@ -2,5 +2,11 @@ namespace ITCS_3112_Lab_1_Checkout.Contracts;
 
 public interface ICheckoutService
 {
-    
+    ICatalog GetCatalog();
+    Receipt Checkout(string itemId, Borrower borrower, DateTime dueDate);
+    Receipt ReturnItem(string itemId);
+    void MarkLost(string itemId);
+    List<CheckoutRecord> ListActiveLoans();
+    List<CheckoutRecord> FindDueSoon(TimeSpan window);
+    List<CheckoutRecord> FindOverdue();
 }
