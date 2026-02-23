3e3cfee4f00c2eefa0bb86e767541ed3594608c2
WilliamSitt <w12501250@gmail.com>
2026-02-22 20:30:00 -0500

    Update Program.cs
    added while loop and switch statement, with one case for exit functionality

--- Program.cs
+++ Program.cs
@@ -4,17 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Equipment Checkout System!");
-        Console.WriteLine("Choose an option:");
-        Console.WriteLine("1. Add items to inventory");
-        Console.WriteLine("2. List available items");
-        Console.WriteLine("3. List unavailable items");
-        Console.WriteLine("4. Checkout item");
-        Console.WriteLine("5. Return item");
-        Console.WriteLine("6. Show due soon (next 24 hours)");
-        Console.WriteLine("7. Show overdue items");
-        Console.WriteLine("8. Search items");
-        Console.WriteLine("9. Mark items as lost");
-        Console.WriteLine("0. Exit");
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Welcome to the Equipment Checkout System!");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Add items to inventory");
+            Console.WriteLine("2. List available items");
+            Console.WriteLine("3. List unavailable items");
+            Console.WriteLine("4. Checkout item");
+            Console.WriteLine("5. Return item");
+            Console.WriteLine("6. Show due soon (next 24 hours)");
+            Console.WriteLine("7. Show overdue items");
+            Console.WriteLine("8. Search items");
+            Console.WriteLine("9. Mark items as lost");
+            Console.WriteLine("0. Exit");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 0: exit = true; break;
+            }
+        }
     }
 }
